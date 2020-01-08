81b0b5c040494054dd8aa95c2358efb583bee15c
samar fares <samarfares1991@gmail.com>
2020-01-08 09:40:45 +0300

    first

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import NavBar from './NavBar';
+import Footer from './Footer';
+import Header from './Header'
+import Container from './Container';
+
+function App() {
+  return (
+    <div >
+      <NavBar />
+      <Header />
+      <Container/>
+      <Footer />
+
+    </div>
+  );
+}
+
+export default App;
